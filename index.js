984f9e69a813a36462c9ae5dc3f964881856b7a2
parson40 <101997352+parsonjd@users.noreply.github.com>
2022-09-19 18:49:26 -0400

    made some minor query changes to my functions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
 
-//Connect database
+//Connect database with my credentials
 const db = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -97,11 +97,9 @@ function mainMenu() {
             //Close application
             case "Exit":
                 console.log("===============================================");
-                console.log("");
-                console.log("   THANK YOU FOR USING THE EMPLOYEE DATABASE   ");
-                console.log("");
+                console.log("                 Goodbye!!  ");
                 console.log("===============================================");
-                connection.end();
+                db.end();
                 break;
         }
     })
@@ -119,8 +117,9 @@ function viewAllDepts() {
         })
 };
 
+//Function to view all roles
 function viewAllRoles() {
-    db.query("SELECT role.id as ID, role.title AS TITLE FROM role",
+    db.query("SELECT role.id as ID, role.title AS TITLE, department.name AS Dept, role.salary FROM role INNER JOIN department ON role.department_id = department.id ORDER BY role.id",
         function (err, res) {
             if (err) throw err
             console.log("");
@@ -131,8 +130,20 @@ function viewAllRoles() {
 
 };
 
+//Function to view all employees
 function viewAllEmps() {
-    db.query("SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS NAME FROM employee",
+    db.query(`SELECT employee.id,
+    employee.first_name,
+    employee.last_name,
+    role.title AS title,
+    department.name AS dept,
+    role.salary,
+    CONCAT(manager.first_name, ' ' ,manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+    ORDER By employee.id`,
         function (err, res) {
             if (err) throw err
             console.log("");
@@ -142,6 +153,7 @@ function viewAllEmps() {
         })
 };
 
+//Function to view all employees by role
 function viewEmpsByRole() {
     db.query("SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS NAME, role.title AS TITLE FROM employee INNER JOIN role on employee.role_id = role.id ORDER BY role.title",
         function (err, res) {
@@ -153,6 +165,7 @@ function viewEmpsByRole() {
         })
 };
 
+//Function to view all employees by department
 function viewEmpsByDept() {
     db.query("SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS NAME, department.name AS DEPT FROM role INNER JOIN employee on employee.role_id = role.id INNER JOIN department on department.id = role.department_id ORDER BY department.name",
         function (err, res) {
@@ -164,6 +177,7 @@ function viewEmpsByDept() {
         })
 }
 
+//Function to view all employees by manager
 function viewEmpsByMan() {
     db.query("SELECT CONCAT(Emp.first_name, ' ', Emp.last_name) AS EMPLOYEE, CONCAT(Man.first_name, ' ', Man.last_name) AS MANAGER FROM Employee Emp INNER JOIN Employee Man on Emp.manager_id = Man.id ORDER BY Emp.last_name",
         function (err, res) {
@@ -174,5 +188,97 @@ function viewEmpsByMan() {
             mainMenu();
         })
 }
+
+//Functin to add new department with prepared statement
+function addDept() {
+    inquirer.prompt([
+        {
+            name: "newDept",
+            type: "input",
+            message: "Please enter the name of the department you want to add."
+        }
+    ]).then((answer) => {
+
+        const sql = `INSERT INTO department (name)
+                VALUES (?)`;
+        const params = [answer.newDept];
+        db.query(sql, params, function (err, res) {
+            if (err) throw err;
+            console.clear();
+            console.log('New department added!');
+            viewAllDepts();
+            mainMenu();
+
+        });
+    });
+};
+
+//Function to add new role with prepared statement
+function addRole() {
+    inquirer.prompt([
+        {
+            name: "title",
+            type: "input",
+            message: "Please enter the title of role you want to add."
+        },
+        {
+            name: "salary",
+            type: "number",
+            message: "Please enter the salary associated with the role you want to add (only numbers)"
+        },
+        {
+            name: "department_id",
+            type: "number",
+            message: "Please enter the department's id associated with the role you want to add."
+        }
+    ]).then((answer) => {
+
+        const sql = `INSERT INTO role (title, salary, department_id)
+                VALUES (?, ?, ?)`;
+        const params = [answer.title, answer.salary, answer.department_id];
+        db.query(sql, params, function (err, res) {
+            if (err) throw err;
+            console.clear();
+            console.log('New role added!');
+            viewAllRoles();
+            mainMenu();
+
+        });
+    });
+};
+
+//Function to add new employee with prepared statement
+function addEmp() {
+    inquirer.prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "Please enter the first name of employee you want to add."
+        },
+        {
+            name: "last_name",
+            type: "input",
+            message: "Please enter the last name of employee you want to add."
+        },
+        {
+            name: "role_id",
+            type: "number",
+            message: "Please enter the role id associated with the employee you want to add (only numbers)."
+        }
+    ]).then((answer) => {
+
+        const sql = `INSERT INTO employee (first_name, last_name, role_id)
+                VALUES (?, ?, ?)`;
+        const params = [answer.first_name, answer.last_name, answer.role_id];
+        db.query(sql, params, function (err, res) {
+            if (err) throw err;
+            console.clear();
+            console.log('New role added!');
+            viewAllEmps();
+            mainMenu();
+
+        });
+    });
+};
 
 
